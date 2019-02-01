f0ba7171f77d80fb409592481e43d9f57d15e3fc
Jayson <jaysonmanas@gmail.com>
2019-02-01 16:16:17 +0200

    welcome message

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My blog',user: 'Great User' });
 });
 
-app.get('/', function(req, res){ 
-  res.render('index',{user: "Great User",title:"homepage"});
-});
+// app.get('/', function(req, res){ 
+//   res.render('index',{user: 'Great User',title:'homepage'});
+// });
 
 module.exports = router;
 /////////////////////////////////
