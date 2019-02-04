38cf4bef0bc2f083e24d1821f92cc8006f8fecc5
Jayson-Manas <jaysonmanas@gmail.com>
2019-02-04 22:40:13 +0200

    replaced app.js file

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
+var createError = require('http-errors');
 var express = require('express');
-var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-
-var Pokemon = require('./db.json');
-
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
-var createRouter = require('./routes/create');
-var viewRouter = require('./routes/view');
-var updateRouter = require('./routes/update');
-var deleteRouter = require('./routes/delete');
+var usersRouter = require('./routes/users');
+var createRouter= require('./routes/create');
+
+
+var app = express();
+// const app = require('express')()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,28 +19,40 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-//bodyParser Middleware
-app.use(bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/create',createRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  //bodyParser Middleware
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //this is not needed if you have bodyParser
 //express middleware
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: false
+  extended: true
 }));
 
-//register routes after middleware
-app.use('/', indexRouter);
-app.use('/create', createRouter);
-app.use('/view', viewRouter);
-app.use('/update', updateRouter);
-app.use('/delete', deleteRouter);
-  
-//app listen on this port
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+module.exports = app;
 app.listen(8080);
 
-module.exports = app;
+
